741e700fa0b7fe45e82846d63307a7c2d8bf1d71
daniel1775 <danilog1000@gmail.com>
2024-11-29 17:17:04 -0500

    Hello Japan

--- ios-app-tutorial/ContentView.swift
+++ ios-app-tutorial/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            /*
+             Image(systemName: "globe")
+                 .imageScale(.large)
+                 .foregroundStyle(.tint)
+             Text("Hello, world! Tenki says hi")
+             */
+            Circle().fill(.red)
         }
         .padding()
     }
