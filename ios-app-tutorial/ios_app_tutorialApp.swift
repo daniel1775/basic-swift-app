77404f39861ba80fccb3ae7d078bda37836ea9f8
daniel1775 <danilog1000@gmail.com>
2024-11-29 16:45:23 -0500

    Initial Commit

---
+++ ios-app-tutorial/ios_app_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_app_tutorialApp.swift
+//  ios-app-tutorial
+//
+//  Created by Daniel on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_app_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-app-tutorialTests/ios_app_tutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_app_tutorialTests.swift
+//  ios-app-tutorialTests
+//
+//  Created by Daniel on 29/11/24.
+//
+
+import Testing
+@testable import ios_app_tutorial
+
+struct ios_app_tutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
