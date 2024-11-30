02c9b7dbcacac90ea4ab7b124c2b3ce04c71e884
daniel1775 <danilog1000@gmail.com>
2024-11-30 15:49:17 -0500

    feat: button and states added

--- ios-app-tutorial/ContentView.swift
+++ ios-app-tutorial/ContentView.swift
@@ -8,23 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selected = "Soccer"
+    @State private var id = 1
+
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash", "Soccer"]
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+    
     var body: some View {
         VStack {
             VStack {
                 Text("Why not try...")
                     .font(.largeTitle.bold())
             }
+            Spacer()
             Circle()
-                .fill(.red)
+                .fill(colors.randomElement() ?? .blue)
                 .padding()
-                .overlay(Image(systemName: "figure.archery")
+                .overlay(Image(systemName: "figure.\(selected.lowercased())")
                     .font(.system(size: 144))
-                    .foregroundColor(.white)
+                    .foregroundColor(colors.randomElement())
                 )
-            Text("Japanese arrow")
+            Spacer()
+            Text("\(selected)")
                 .font(.title)
+            Button("Change it!") {
+                withAnimation(.easeInOut(duration: 0.3)) {
+                    selected = activities.randomElement() ?? "Squash"
+                    id += 1
+                }
+            }.buttonStyle(.borderedProminent)
         }
         .padding()
+        .transition(.slide)
+        .id(id)
     }
 }
 
